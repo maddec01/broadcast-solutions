c1f36e008734c8f9be8527e356bf9178a10ed753
Pedro Gomes <pedrofacg@gmail.com>
2019-02-03 22:23:28 +0100

    localstorage user

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { compose } from 'recompose';
 
 import { withFirebase } from '../Firebase';
+import { withAuthorization } from '../Session';
+import * as ROLES from '../../constants/roles';
 
 class AdminPage extends Component {
   constructor(props) {
@@ -41,6 +44,10 @@ class AdminPage extends Component {
       <div>
         <h1>Admin</h1>
 
+        <p>
+          The Admin Page is accessible by every signed in admin user.
+        </p>
+
         {loading && <div>Loading ...</div>}
 
         <UserList users={users} />
@@ -67,4 +74,5 @@ const UserList = ({ users }) => (
   </ul>
 );
 
-export default withFirebase(AdminPage);
+const condition = authUser => authUser && authUser.roles.includes(ROLES.ADMIN);
+export default compose(withAuthorization(condition),withFirebase)(AdminPage);
